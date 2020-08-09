2261e4c698b5f35307ac095a429dc2317731c3ed
mbstalli <mbstalli@calpoly.edu>
2020-08-09 12:52:03 -0700

    added win condition, need to add reset button and finish up

--- minesweeper.py
+++ minesweeper.py
@@ -8,6 +8,7 @@ HEIGHT = 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Minesweeper")
 font = pygame.font.SysFont('Arial', 30, True)
+font2 = pygame.font.SysFont('Arial', 60, True)
 
 #grid array
 grid = []
@@ -36,16 +37,16 @@ class cell(object):
 		self.bombAround = 0
 		self.flag = False
 		#randomly determines bomb location
-		if(random.randint(0, cellcount) < round(cellcount / 8)):
+		if(random.randint(0, cellcount) < round(cellcount / 12)):
 			self.bomb = True
 		else:
 			self.bomb = False
 
 #function to search for and return a cell in the "grid" list
 def searchForCell(xcoord, ycoord, grid):
-	for entry in grid:
-		if entry.x == xcoord and entry.y == ycoord:
-			return entry
+	for cell in grid:
+		if cell.x == xcoord and cell.y == ycoord:
+			return cell
 
 #function to search 8 tiles around space for bombs
 def searchForBombs(xcoord, ycoord, grid):
@@ -89,8 +90,8 @@ def searchForBombs(xcoord, ycoord, grid):
 #goes through list and looks at specific space to determine if bomb or not
 def searchList(xcoord, ycoord, grid):
 	bombcount = 0
-	for entry in grid:
-		if entry.x == xcoord and entry.y == ycoord and entry.bomb == True:
+	for cell in grid:
+		if cell.x == xcoord and cell.y == ycoord and cell.bomb == True:
 			bombcount = 1
 	return bombcount
 
@@ -167,16 +168,22 @@ def revealEmptyCells(grid, xcoord, ycoord):
 
 #helper function for revealEmptyCells to reveal a cell in the grid and set its bombAround
 def searchAndReveal(xcoord, ycoord, grid, bombcount):
-	for space in grid:
-		if xcoord == space.x and ycoord == space.y:
-			space.revealed = True
-			space.bombAround = bombcount
+	for cell in grid:
+		if xcoord == cell.x and ycoord == cell.y:
+			cell.revealed = True
+			cell.bombAround = bombcount
 
 #helper function for revealEmptyCells to reveal a cell in the grid
 def revealList(xcoord, ycoord, grid):
-	for entry in grid:
-		if entry.x == xcoord and entry.y == ycoord:
-			entry.revealed = True
+	for cell in grid:
+		if cell.x == xcoord and cell.y == ycoord:
+			cell.revealed = True
+
+#function to reveal all bombs when one is clicked`
+def revealAllBombs(grid):
+	for cell in grid:
+		if cell.bomb:
+			cell.revealed = True
 
 # setting up grid by adding cells to list
 # each list has an x and y coordinate in the grid
@@ -208,6 +215,11 @@ while running:
 	cellsToReveal = []
 	needToAdd = True
 
+	cellsRevealed = 0
+
+	isBomb = False
+	gameWon = False
+
 	xcoord = 0
 	ycoord = 0
 
@@ -227,12 +239,12 @@ while running:
 					ycoord += 1
 					posy -= 25
 
-				for tempcell in grid:
-					if xcoord == tempcell.x and ycoord == tempcell.y:
-						if tempcell.flag == False:
-							tempcell.flag = True
+				for cell in grid:
+					if xcoord == cell.x and ycoord == cell.y:
+						if cell.flag == False:
+							cell.flag = True
 						else:
-							tempcell.flag = False
+							cell.flag = False
 			#left click
 			else:
 				while posx >= 25:
@@ -245,73 +257,102 @@ while running:
 
 				bombcount = searchForBombs(xcoord, ycoord, grid)
 
+				#check to see if bomb before checking adjacent cells
+				for cell in grid:
+					if xcoord == cell.x and ycoord == cell.y and cell.bomb:
+						isBomb = True
+
+				if isBomb:
+					revealAllBombs(grid)
+
 				#functionality to find adjacent empty cells and reveal them
-				if bombcount == 0:
+				if bombcount == 0 and not isBomb:
+					#list of adjacent cells that are empty
 					toExpand = revealEmptyCells(grid, xcoord, ycoord)
 					#look through adjacent empty cells until there are no more
 					while needToAdd:
 						needToAdd = False
 						toExpand, temp = [], toExpand
 
-						for entry in temp:
-							cellsToClear = revealEmptyCells(grid, entry.x, entry.y)
-							for e in cellsToClear:
-								if not e.bomb and e not in cellsToReveal:
-									cellsToReveal.append(e)
-									if e.bombAround == 0:
-										toExpand.append(e)
+						for cell in temp:
+							cellsToClear = revealEmptyCells(grid, cell.x, cell.y)
+							for entry in cellsToClear:
+								#if there are adjacent cells that have not been added, add them to be revealed
+								if not entry.bomb and entry not in cellsToReveal:
+									cellsToReveal.append(entry)
+									#and if that entry has no adjacent bombs, then add it to toExpand so that we can reveal its adjacent cells
+									if entry.bombAround == 0:
+										toExpand.append(entry)
 										needToAdd = True
 
 					#once we have found all adjacent empty cells, reveal them and their neighbors
-					for entry in cellsToReveal:
-						revealList(entry.x, entry.y, grid)
+					for cell in cellsToReveal:
+						revealList(cell.x, cell.y, grid)
 
 				#reveal the original cell you clicked on no matter if it is has 0 adjacent bombs or not
-				for space in grid:
-					if xcoord == space.x and ycoord == space.y:
-						space.revealed = True
-						space.bombAround = bombcount
+				if not isBomb:
+					for cell in grid:
+						if xcoord == cell.x and ycoord == cell.y:
+							cell.revealed = True
+							cell.bombAround = bombcount
+
+				#find out how many cells are revealed in the grid
+				for cell in grid:
+					if cell.revealed:
+						cellsRevealed += 1
+
+				if cellsRevealed == cellcount - mines:
+					gameWon = True
+				
 
 	#fill the screen with white
 	win.fill((255, 255, 255))
 
-	text = font.render("Mines: " + str(mines), 1, (0, 0, 0))
-	win.blit(text, (20, 10))
+	if gameWon:
+		text = font2.render("You win!", 1, (0, 0, 0))
+		textRect = text.get_rect(center = (WIDTH/2, HEIGHT/2))
+		win.blit(text, textRect)
+		pygame.display.update()
+		time.sleep(4)
+		break
+	else:
+		text = font.render("Mines: " + str(mines), 1, (0, 0, 0))
+		win.blit(text, (20, 10))
 
-	x = 25
-	y = 50
+		x = 25
+		y = 50
 
-	#image processing for the screen
-	for i in grid:
-		#if you have clicked on the cell
-		if i.revealed == True and i.flag == False:
-			#if it is a bomb
-			if i.bomb:
-				win.blit(mine, (x, y))
-			#if there are no adjacent bombs
-			elif i.bombAround == 0:
-				win.blit(empty, (x, y))
-			#if there are bombs around, show the amount
+		#image processing for the screen
+		for i in grid:
+			#if you have clicked on the cell
+			if i.revealed == True and i.flag == False:
+				#if it is a bomb
+				if i.bomb:
+					win.blit(mine, (x, y))
+				#if there are no adjacent bombs
+				elif i.bombAround == 0:
+					win.blit(empty, (x, y))
+				#if there are bombs around, show the amount
+				else:
+					win.blit(numbers[i.bombAround - 1], (x, y))
+				x += 25
+				if i.x % column == 0:
+					y += 25
+					x = 25
+			#if you have right-clicked on a cell, a flag will appear
+			elif i.flag == True:
+				win.blit(flag, (x, y))
+				x += 25
+				if i.x % column == 0:
+					y += 25
+					x = 25
+			#if you haven't clicked on the cell at all
 			else:
-				win.blit(numbers[i.bombAround - 1], (x, y))
-			x += 25
-			if i.x % column == 0:
-				y += 25
-				x = 25
-		#if you have right-clicked on a cell, a flag will appear
-		elif i.flag == True:
-			win.blit(flag, (x, y))
-			x += 25
-			if i.x % column == 0:
-				y += 25
-				x = 25
-		#if you haven't clicked on the cell at all
-		else:
-			win.blit(tile, (x, y))
-			x += 25
-			if i.x % column == 0:
-				y += 25
-				x = 25
+				win.blit(tile, (x, y))
+				x += 25
+				if i.x % column == 0:
+					y += 25
+					x = 25
 
 	pygame.display.update()
 
